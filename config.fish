763639e114e60390e8219835ee80db34b83f7f89
Phydon <84594612+Phydon@users.noreply.github.com>
2022-06-22 18:47:12 +0200

    Add files via upload

--- config.fish
+++ config.fish
@@ -2,6 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+function fish_greeting
+	echo ...
+	echo ">>" (set_color yellow) AYY AYY CAPTAIN (set_color normal)
+	echo ..
+	echo The time is (set_color red; date +%T; set_color normal) and this machine is called (set_color blue) $hostname
+	echo .
+end
+
+
 alias sf="~/Aliases/sf"
 alias rep="~/Aliases/rep"
 alias cat="bat"
@@ -13,3 +23,6 @@ alias ps="procs --sortd cpu"
 function skrg
     command sk -e --preview "rg -i -p $argv {}"
 end
+
+
+starship init fish | source
