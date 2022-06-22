211b414a3a716a7e0a9053a8b5f3ff35413ec0b6
Phydon <84594612+Phydon@users.noreply.github.com>
2022-06-22 10:17:39 +0200

    Add files via upload

--- config.fish
+++ config.fish
@@ -7,6 +7,8 @@ alias rep="~/Aliases/rep"
 alias cat="bat"
 alias ls="exa -h --long --icons --time-style=long-iso"
 alias fd="fd -H"
+alias du="dust"
+alias ps="procs --sortd cpu"
 
 function skrg
     command sk -e --preview "rg -i -p $argv {}"
