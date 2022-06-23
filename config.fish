b0acede3791952204bd97b884d07d52fcf2ebae6
Phydon <84594612+Phydon@users.noreply.github.com>
2022-06-23 08:26:58 +0200

    Add files via upload

--- config.fish
+++ config.fish
@@ -3,12 +3,17 @@ if status is-interactive
 end
 
 
+# function fish_greeting
+# 	echo ...
+# 	echo ">>" (set_color yellow) AYY AYY CAPTAIN (set_color normal)
+# 	echo ..
+# 	echo The time is (set_color red; date +%T; set_color normal) and this machine is called (set_color blue) $hostname
+# 	echo .
+# end
+
 function fish_greeting
-	echo ...
-	echo ">>" (set_color yellow) AYY AYY CAPTAIN (set_color normal)
-	echo ..
-	echo The time is (set_color red; date +%T; set_color normal) and this machine is called (set_color blue) $hostname
-	echo .
+	command figlet WELCOME | lolcat
+	command fortune | cowsay -f $(exa ~/Documents/cows/|shuf -n 1) | lolcat
 end
 
 
