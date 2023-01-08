2ddf24c305e697394ea4a3b38e89b9f7d44748e6
Phydon <84594612+Phydon@users.noreply.github.com>
2023-01-08 17:00:38 +0100

    Add files via upload

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,50 +1,18 @@
-# Import-Module oh-my-posh
-
-# oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/jandedobbeleer.omp.json | Invoke-Expression
-
-# minimal (without username):
-# Set-PoshPrompt -Theme material
-# Set-PoshPrompt -Theme pararussel
-# Set-PoshPrompt -Theme lambda
-
-# almost minimal (with username):
-# Set-PoshPrompt -Theme zash
-
-# less minimal:
-# Set-PoshPrompt -Theme xtoys
-# Set-PoshPrompt -Theme huvix
-# Set-PoshPrompt -Theme pure
-# GOOD ONE: nordtron
-# Set-PoshPrompt -Theme nordtron
-# Set-PoshPrompt -Theme darkblood
-# Set-PoshPrompt -Theme emodipt
-# Set-PoshPrompt -Theme negligible
-# Set-PoshPrompt -Theme ys
-# Set-PoshPrompt -Theme sorin
-
-# not minimal:
-# Set-PoshPrompt -Theme atomicBit
-# Set-PoshPrompt -Theme powerlevel10k_lean
-# Set-PoshPrompt -Theme peru
-# Set-PoshPrompt -Theme stelbent.minimal
-# Set-PoshPrompt -Theme blue-owl
-# Set-PoshPrompt -Theme night-owl
-# Set-PoshPrompt -Theme slimfat
-# Set-PoshPrompt -Theme jv_sitecorian
-
-
 # generate new aliases for specific programs
-New-Alias -Name sf -Value C:/Aliases/Programs/sf.exe 
-New-Alias -Name fzf -Value C:/Aliases/Programs/fzf.exe 
-New-Alias -Name bat -Value C:/Aliases/Programs/bat.exe 
-New-Alias -Name delta -Value C:/Aliases/Programs/delta.exe 
-New-Alias -Name rg -Value C:/Aliases/Programs/rg.exe 
+# New-Alias -Name sf -Value C:/Aliases/Programs/sf.exe 
+# New-Alias -Name fzf -Value C:/Aliases/Programs/fzf.exe 
+# New-Alias -Name bat -Value C:/Aliases/Programs/bat.exe 
+# New-Alias -Name delta -Value C:/Aliases/Programs/delta.exe 
+# New-Alias -Name rg -Value C:/Aliases/Programs/rg.exe 
 
 Function Find {gci -r -erroraction 'silentlycontinue' | where Name -match $args[0] | select FullName}
 New-Alias -Name fd -Value Find
 
 Function cdback {cd ..}
 New-Alias -Name .. -Value cdback
+
+Function vimquit {exit}
+New-Alias -Name :q -Value vimquit
 
 
 # # greeting function
