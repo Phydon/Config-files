20fb6b41c9b849888c2f873d34957ed2a133fd1e
Phydon <84594612+Phydon@users.noreply.github.com>
2026-04-21 15:08:54 +0000

    Add files via upload

--- fish/functions/sysinfo.fish
+++ fish/functions/sysinfo.fish
@@ -4,6 +4,8 @@ function sysinfo --description "Display system information"
     echo "Hostname: "(hostname)
     echo "User: "$USER
     echo "OS: "(uname -s)
+    echo "Distro: "(uname -a | cut -d' ' -f7)
+    echo "Arch: "(uname -m)
     echo "Kernel: "(uname -r)
     echo "Uptime: "(uptime | cut -d',' -f1 | cut -d' ' -f4-)
     echo "Memory: "(free -h | grep '^Mem:' | awk '{print $3"/"$2}')
